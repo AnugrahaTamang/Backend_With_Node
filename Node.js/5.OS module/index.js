7912e22f5ab694784189f03213df8c7aee4450e8
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-04-02 12:26:42 +0545

    os module

--- Node.js/5.OS module/index.js
+++ Node.js/5.OS module/index.js
@@ -5,4 +5,4 @@ console.log(os.cpus());
 console.log(os.homedir()); //C:\Users\Lenovo
 // console.log(os.networkInterfaces());
 console.log(os.freemem()); //1328455680
-console.log(os.totalmem());
+console.log(os.totalmem()); //8480419840
