d48603708ec185463624a0a9a16ab56bb40987a3
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-04-02 12:25:25 +0545

    os module

---
+++ Node.js/5.OS module/index.js
@@ -0,0 +1,8 @@
+import os from "os";
+console.log(os.hostname()); //DESKTOP-BENV5AM
+console.log(os.platform()); //32
+console.log(os.cpus());
+console.log(os.homedir()); //C:\Users\Lenovo
+// console.log(os.networkInterfaces());
+console.log(os.freemem()); //1328455680
+console.log(os.totalmem());
