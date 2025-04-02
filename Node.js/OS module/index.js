ee6f98c5c3fdc57c7556bfba7e9621b748d61bfd
AnugrahaTamang <166694801+AnugrahaTamang@users.noreply.github.com>
2025-04-02 12:27:31 +0545

    Delete Node.js/OS module directory

--- Node.js/OS module/index.js
+++
@@ -1,8 +0,0 @@
-import os from "os";
-console.log(os.hostname()); //DESKTOP-BENV5AM
-console.log(os.platform()); //32
-console.log(os.cpus());
-console.log(os.homedir()); //C:\Users\Lenovo
-// console.log(os.networkInterfaces());
-console.log(os.freemem()); //1328455680
-console.log(os.totalmem());
