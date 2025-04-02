2043e75c1ceee5b962c8f875ff8fa2b374367c1e
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-04-02 12:35:31 +0545

    url

--- Node.js/6.URL module/index.js
+++ Node.js/6.URL module/index.js
@@ -3,4 +3,4 @@ const myurl = new URL("https://www.example.com:2000/p/n");
 console.log(myurl.host); //www.example.com:2000
 console.log(myurl.port); //2000
 console.log(myurl.href); //https://www.example.com:2000/p/n
-console.log(myurl.protocol);
+console.log(myurl.protocol); //https:
