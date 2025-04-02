d4b80c1254ca9a09f429f079c21ec0cf7e6f1ada
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-04-02 12:33:20 +0545

    url module

---
+++ Node.js/6.URL module/index.js
@@ -0,0 +1,6 @@
+import { URL } from "url";
+const myurl = new URL("https://www.example.com:2000/p/n");
+console.log(myurl.host); //www.example.com:2000
+console.log(myurl.port); //2000
+console.log(myurl.href); //https://www.example.com:2000/p/n
+console.log(myurl.protocol);
